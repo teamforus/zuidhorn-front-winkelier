37bf145a1f30e2d61b73bf77fce31b5eeae12252
HardCoderNET <valik432@gmail.com>
2017-10-30 07:48:13 +0200

    rebuilding site Mon Oct 30 07:48:13 EET 2017

--- assets/js/app.js
+++ assets/js/app.js
@@ -860,6 +860,7 @@ shopkeeperApp.provider('ApiRequest', function() {
                     var credentails = CredentialsService.get();
 
                     return {
+                        'Locale': 'nl',
                         'Accept': 'application/json',
                         'Authorization': 'Bearer ' + (credentails ? credentails.access_token : ''),
                         'Device-Id': DeviceIdService.getDeviceId().id,
