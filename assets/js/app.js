3d8c7d5dabc66db3a4410b3bba63a1297a3836b0
HardCoderNET <valik432@gmail.com>
2017-11-06 23:56:30 +0200

    rebuilding site Mon Nov  6 23:56:30 EET 2017

--- assets/js/app.js
+++ assets/js/app.js
@@ -963,36 +963,48 @@ shopkeeperApp.directive('contactForm', [
         };
     }
 ]);
-shopkeeperApp.directive('googleAddress', [function() {
-    return {
-        restrict: 'A',
-        replace: true,
-        transclude: true,
-        scope: true,
-        link: function($scope, iElm, iAttrs, controller) {
-            function initAutocomplete() {
-                // Create the autocomplete object, restricting the search to geographical
-                // location types.
-                autocomplete = new google.maps.places.Autocomplete(
-                    /** @type {!HTMLInputElement} */
-                    iElm[0], {
-                        types: ['geocode']
+shopkeeperApp.directive('googleAddress', [
+    '$q',
+    function(
+        $q
+    ) {
+        return {
+            restrict: 'A',
+            replace: true,
+            transclude: true,
+            scope: {
+                ngModel: '='
+            },
+            link: function($scope, iElm, iAttrs, controller) {
+                function initAutocomplete() {
+                    // Create the autocomplete object, restricting the search to geographical
+                    // location types.
+                    autocomplete = new google.maps.places.Autocomplete(
+                        iElm[0], {
+                            types: ['geocode']
+                        });
+
+                    // When the user selects an address from the dropdown, populate the address
+                    // fields in the form.
+                    $(iElm[0]).bind('keydown', function(event) {
+                        if (event.keyCode == 13) {
+                            event.preventDefault();
+                            return false;
+                        }
                     });
 
-                // When the user selects an address from the dropdown, populate the address
-                // fields in the form.
-                $(iElm[0]).bind('keydown', function(event) {
-                    if (event.keyCode == 13) {
-                        event.preventDefault();
-                        return false;
-                    }
-                });
-            }
+                    google.maps.event.addListener(autocomplete, 'place_changed', function(e) {
+                        $scope.$apply(function() {
+                            $scope.ngModel = iElm[0].value;
+                        });
+                    });
+                }
 
-            initAutocomplete();
-        }
-    };
-}]);
+                initAutocomplete();
+            }
+        };
+    }
+]);
 shopkeeperApp.directive('panelHeader', [
     'ContactFormService',
     'FormBuilderService',
